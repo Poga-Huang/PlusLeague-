daae8044e1df719c400d6486a454b8077b0e5f6c
poga <s15114263@stu.edu.tw>
2022-06-24 21:16:48 +0800

    Initial Commit

---
+++ PlusLeague+/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlusLeague+
+//
+//  Created by 黃柏嘉 on 2022/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
